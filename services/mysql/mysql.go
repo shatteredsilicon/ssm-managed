3e2c68321aa2a7e1b45e5c7b6f6348b4e01acf37
Jason Ng <oblitorum@gmail.com>
2024-11-04 01:35:39 +0800

    Disable also perf_schema.eventswaits when tablestats should be disabled

--- services/mysql/mysql.go
+++ services/mysql/mysql.go
@@ -292,7 +292,7 @@ func (svc *Service) addMySQLdExporter(ctx context.Context, tx *reform.TX, servic
 		err = db.QueryRowContext(sqlCtx, "SELECT COUNT(*) FROM information_schema.tables").Scan(&tableCount)
 		cancel()
 		db.Close()
-		agent.MySQLDisableTablestats = pointer.ToBool(tableCount > 1000)
+		agent.MySQLDisableTablestats = pointer.ToBool(tableCount > int(services.DisableTablestatsLimit()))
 	}
 	if err != nil {
 		if err, ok := err.(*mysql.MySQLError); ok {
@@ -333,7 +333,6 @@ func (svc *Service) mysqlExporterCfg(agent *models.MySQLdExporter, dsn string) *
 		"-collect.info_schema.processlist",
 		"-collect.info_schema.query_response_time",
 		"-collect.info_schema.userstats",
-		"-collect.perf_schema.eventswaits",
 		"-collect.perf_schema.file_events",
 		"-collect.slave_status",
 		fmt.Sprintf("-collect.auto_increment.columns=%s", tableStatsValue),
@@ -342,6 +341,7 @@ func (svc *Service) mysqlExporterCfg(agent *models.MySQLdExporter, dsn string) *
 		fmt.Sprintf("-collect.perf_schema.indexiowaits=%s", tableStatsValue),
 		fmt.Sprintf("-collect.perf_schema.tableiowaits=%s", tableStatsValue),
 		fmt.Sprintf("-collect.perf_schema.tablelocks=%s", tableStatsValue),
+		fmt.Sprintf("-collect.perf_schema.eventswaits=%s", tableStatsValue),
 		fmt.Sprintf("-web.listen-address=127.0.0.1:%d", *agent.ListenPort),
 		"-web.auth-file=\"\"",
 	}
--- services/vars.go
+++ services/vars.go
@@ -2,19 +2,34 @@ package services
 
 import (
 	"os"
+	"strconv"
 	"time"
 )
 
-// sqlCheckTimeout maximum time for connecting to the database and running all queries
-var sqlCheckTimeout = 5 * time.Second
+var (
+	// sqlCheckTimeout maximum time for connecting to the database and running all queries
+	sqlCheckTimeout = 5 * time.Second
+
+	// disableTablestatsLimit the table amount limit for deciding whether to collect tablestats metrics
+	disableTablestatsLimit uint16 = 1000
+)
 
 func init() {
 	timeout, _ := time.ParseDuration(os.Getenv("SSM_SQL_CHECK_TIMEOUT"))
 	if timeout > 0 {
 		sqlCheckTimeout = timeout
 	}
+
+	tablestatsLimit, err := strconv.ParseUint(os.Getenv("SSM_DISABLE_TABLESTATS_LIMIT"), 10, 16)
+	if err == nil {
+		disableTablestatsLimit = uint16(tablestatsLimit)
+	}
 }
 
 func SQLCheckTimeout() time.Duration {
 	return sqlCheckTimeout
+}
+
+func DisableTablestatsLimit() uint16 {
+	return disableTablestatsLimit
 }
